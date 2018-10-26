2efb2b688a76354040acacaacdae5daa830fbe0e
nmns93 <Nana.mensah1@nhs.net>
2018-10-26 11:52:06 +0100

    update_index.py now writes file. Updates comments in code.sh

--- resources/home/dnanexus/update_index.py
+++ resources/home/dnanexus/update_index.py
@@ -4,15 +4,20 @@
 Update Multiqc index file from the Viapath Genomics Server with link to new multiqc report.
 
 Example:
-    $ python update_index.py old_index.html report-multiqc.html > new_index.html
+    $ python update_index.py old_index.html report-multiqc.html
+Output:
+    new_index.html
 """
 
 import os
 import sys
 from bs4 import BeautifulSoup as Soup
 
-# Parse script arguments
+# Script arguments are loaded to the sys.argv list. The expected first item is the index html file
+# and the second is the multqc html
 index_html = sys.argv[1]
+# Remove directory prefix from multiqc html input argument
+# E.g. /usr/bin/multiqc.html --> multiqc.html
 multiqc_html = os.path.basename(sys.argv[2])
 
 # Open the index file as a BeautifulSoup.Soup class. This reads the HTML tags as python objects.
@@ -28,7 +33,12 @@ ul_tag = soup.new_tag('ul')
 a_tag = soup.new_tag('a', href="./reports/{}".format(multiqc_html))
 
 # Set HTML tag metadata
-# Note: The div 'class' attribute defines how Bootstrap3 will render the page
+# Note: The div 'class' attribute defines how Bootstrap3 will render the page. 
+# Example HTML:
+# <div class="well">
+#   <h1>REPORT</h1>
+#   <ul><a href="./reports/REPORT-multiqc.html">MultiQC report</a></ul>
+# </div>
 div_tag['class'] = "well"
 h1_tag.string=multiqc_html.rstrip('-multiqc.html')
 a_tag.string="MultiQC report"
@@ -38,10 +48,15 @@ ul_tag.append(a_tag)
 div_tag.append(h1_tag)
 div_tag.append(ul_tag)
 
-# Insert the HTML tags into the index file, nested within the <body> tag
+# Insert the HTML tags into the index file, nested within the <body> tag.
+# As soup.body is a list of HTML tags nested within the HTML body. The first argument of the
+# soup.body.insert() method dictates where new tags should be inserted in this list. To ensure the
+# new multiqc report link appears at the top of the HTML, we insert our <br>, <div> and <br> tags in
+# the 1st, 2nd, and 3rd position within the body tag.
 soup.body.insert(1, br_tag)
 soup.body.insert(2, div_tag)
 soup.body.insert(3, br_tag2)
 
-# Print the new index to the stdout stream
-print(soup)
+# Write the new index file
+with open('new_index.html', 'w') as file:
+	file.write(str(soup))
--- src/code.sh
+++ src/code.sh
@@ -26,19 +26,25 @@ ssh_opts='-q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /hom
 #   -z use compression in data transfer, -e SSH command with options
 rsync -avhz -e "ssh $ssh_opts" mokaguys@genomics.viapath.co.uk:/var/www/html/mokaguys/multiqc/index.html ${out_dir}/old_index.html
 
-# Copy HTML file to the server if it does not exist in the correct location
+# Create a variable containing the SSH command arguments required to determine if the file exists. 
+# These arguments include the options to connect to the server and the bash 'test' command, with the
+# expected multiqc report file path passed to the -e flag for testing.
 file_exists_test="$ssh_opts mokaguys@genomics.viapath.co.uk test -e /var/www/html/mokaguys/multiqc/reports/${multiqc_html_name}"
+
+# Test if the file exists on the Viapath Genomics server using ssh and ${file_exist_test} arguments.
 if ssh ${file_exists_test}; then
-   echo "File exists"
+    echo "File exists."
+# If the file does not exist
 else
-   rsync -avhz -e "ssh $ssh_opts" ${multiqc_html_path} mokaguys@genomics.viapath.co.uk:/var/www/html/mokaguys/multiqc/reports/${multiqc_html_name}
+    # Upload the multiqc html to the multiqc reports directory
+    rsync -avhz -e "ssh $ssh_opts" ${multiqc_html_path} mokaguys@genomics.viapath.co.uk:/var/www/html/mokaguys/multiqc/reports/${multiqc_html_name}
+    # Call a python script to create a new index.html from html file list. This script outputs the file
+    # 'new_index.html' to the current working directory.
+    python update_index.py ${out_dir}/old_index.html ${multiqc_html_path}
+    # Upload new index.html to server
+    rsync -avhz -e "ssh $ssh_opts" ${out_dir}/new_index.html mokaguys@genomics.viapath.co.uk:/var/www/html/mokaguys/multiqc/index.html
 fi
 
-# Call python script to create a new index.html from html file list
-python update_index.py ${out_dir}/old_index.html ${multiqc_html_path} > ${out_dir}/new_index.html
-
-# Upload new index.html to server
-rsync -avhz -e "ssh $ssh_opts" ${out_dir}/new_index.html mokaguys@genomics.viapath.co.uk:/var/www/html/mokaguys/multiqc/index.html
-
-# Upload outputs to DNAnexus
+# Upload outputs to DNAnexus. This will always upload 'old_index.html'. If the multiqc report did
+# not exist, it will also upload 'new_index.html'.
 dx-upload-all-outputs
